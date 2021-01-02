006b11b924ef427c7ef262b1bcbe5cb7905834a7
Ayano Hara <ayano0127@gmail.com>
2021-01-02 18:32:20 +0900

    first commit

---
+++ Study_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Study_SwiftUI
+//
+//  Created by AYANO HARA on 2021/01/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
